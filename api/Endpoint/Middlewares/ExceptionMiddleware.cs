0a7623ab4022bbcab1786dbcb0442616a6e85d90
dnErf <frend.fernando@live.com>
2020-08-22 14:38:49 -0700

    h retrigger 20082203

--- api/Endpoint/Middlewares/ExceptionMiddleware.cs
+++ api/Endpoint/Middlewares/ExceptionMiddleware.cs
@@ -35,7 +35,7 @@ namespace Endpoint.Middlewares
 
                 var response = _env.IsDevelopment()
                     ? new ApiException((int) HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
-                    : new ApiException((int) HttpStatusCode.InternalServerError);
+                    : new ApiException((int) HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString());
 
                 var json = JsonSerializer.Serialize(
                     response, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }
